b75911591e06aa57dc0b0540c2f13f337796489e
OumouK <40306909+OumouK@users.noreply.github.com>
2018-12-09 14:31:41 +0100

    Update Panier.java

--- src/laFac/Panier.java
+++ src/laFac/Panier.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class Panier {
     private ArrayList<Produit> contenu;
-    private float total;
+    private double total;
 
     public ArrayList<Produit> getContenu() {
         return this.contenu;
@@ -16,22 +16,23 @@ public class Panier {
 
     public Panier() {
         this.contenu = new ArrayList<Produit>();
+	this.total=0; //O
     }
 
     public Panier(ArrayList<Produit> contenu) {
         this.contenu = contenu;
     }
 
-	public float getTotal() {
+	public double getTotal() {
 		return total;
 	}
 
-	public void setTotal(float t) {
+	public void setTotal(double t) {
 		this.total = t;
 	}
 	
-	public float total() {
-		float t=0.f;
+	public double total() {
+		double t=0.;
 		if(contenu.size()!=0) {
 			for(Produit p: contenu) {
 			t+=p.getPrix();
@@ -39,5 +40,10 @@ public class Panier {
 		}
 		return t; 
 	}
+	//o:ajout article
+	public void ajoutArticle(Produit p){
+		this.contenu.add(p);
+	}
+	public void alerte(){}
 }
 
