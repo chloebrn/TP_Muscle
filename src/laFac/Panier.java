5ab42356608d379a2d4e299b6c96d48496a0ba1a
LayeThiate <39412207+LayeThiate@users.noreply.github.com>
2018-12-13 01:42:58 +0100

    Update Panier.java

--- src/laFac/Panier.java
+++ src/laFac/Panier.java
@@ -5,6 +5,18 @@ import java.util.ArrayList;
 public class Panier {
     private ArrayList<Produit> contenu;
     private double total;
+	private IObserveur dir; // ABDOULAYE
+	
+	//ABDOULAYE
+	public Panier(IObserveur io) {
+		this();
+		dir = io;
+	}
+
+	// ABDOULAYE
+	public void ajouterProduit(Produit p) {
+		contenu.add(p);
+	}
 
     public ArrayList<Produit> getContenu() {
         return this.contenu;
@@ -36,10 +48,26 @@ public class Panier {
 		if(contenu.size()!=0) {
 			for(Produit p: contenu) {
 			t+=p.getPrix();
+			
+			alerteProduit();  //ABDOULAYE
+			alerteCombinProd(); //ABDOULAYE
 		}
 		}
 		return t; 
 	}
+	
+	// ABOULAYE
+	public void alerteProduit() {
+		for (Produit p : contenu) {
+			dir.majProduit(p);
+		}
+	}
+
+	// ABDOULAYE
+	public void alerteCombinProd() {
+		dir.majCombProd(contenu);
+	}
+	
 	//o:ajout article
 	public void ajoutArticle(Produit p){
 		this.contenu.add(p);
