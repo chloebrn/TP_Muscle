ef540c1e1d2e19afec72f49c5b08915782c75b5a
chloebrn <44924305+chloebrn@users.noreply.github.com>
2018-12-13 20:22:03 +0100

    Update MembrePersonnel.java

--- src/laFac/MembrePersonnel.java
+++ src/laFac/MembrePersonnel.java
@@ -2,12 +2,18 @@ package laFac;
 
 import java.util.ArrayList;
 
-public class MembrePersonnel extends Statut {
+public class MembrePersonnel implements Statut {
     private String mailPerso;
     private String mdpPerso;
     private ArrayList<OffreMembre> sesReductions;
 
+    
     public MembrePersonnel() {
+    	mailPerso="undefined";
+    	sesReductions=new ArrayList<>();
+    }
+    public MembrePersonnel(String id) {
+    	mailPerso=id;
     }
 
 	public String getMailPerso() {
@@ -33,4 +39,26 @@ public class MembrePersonnel extends Statut {
 	public void setSesReductions(ArrayList<OffreMembre> sesReductions) {
 		this.sesReductions = sesReductions;
 	}
+	
+    @Override
+    public void sonStatut(Client c) {
+        c.setSonStat(this);
+
+    }
+
+    @Override
+    public String toString() {
+        return "Le statut du client est MembrePersonnel";
+    }
+    
+    public boolean equals(Object o) {
+    	if(o==this) return true;
+    	if(o==null) return false;
+    	if(o instanceof MembrePersonnel) {
+    		MembrePersonnel m=(MembrePersonnel) o;
+    		return m.mailPerso==this.mailPerso;
+    	}
+		return false;
+    }
+    
 }
