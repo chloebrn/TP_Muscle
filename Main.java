b2987656dd612ae248935ed13bfd63b2638f0c04
chloebrn <44924305+chloebrn@users.noreply.github.com>
2018-12-15 13:43:35 +0100

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,29 +1,41 @@
 package laFac;
 
+import java.util.ArrayList;
+
 public class Main {
 
 	public static void main(String[] args) {
-		Client anne=new Client();
+		Client anne=new Client("anne");
+		Client fred= new Client("fred");
+		Client cha=new Client("cha");
+		MembrePersonnel sPAnne=new MembrePersonnel("anneperso");
+		Adherent sAAnne=new Adherent("anneadherent");
+		MembrePersonnel sPFred=new MembrePersonnel("fredperso");
+		Client.addClient(sPAnne);
+		Client.addClient(sPFred);
+		Client.addClient(sAAnne);
+		ArrayList<CarteDeFidelite> sesCartes=new ArrayList<>();
+		CarteDeFidelite c1=new CarteDeFidelite(15);
+		sesCartes.add(c1);
+		sAAnne.setSesCartes(sesCartes);
+		Tele tv=new Tele("tv", 400.d, 20);
+		OffreProduit o=new OffreProduit(0.1d, tv);
+		
+
+		
 		Livre l1=new Livre("Harry Poter", 50, 10, "J K Roling");
 		Livre l2=new Livre();
-
-		/*Adherent startState = new Adherent();
-		startState.sonStatut(anne);
-
+		
 		System.out.println(anne.getSonStat().toString());
-
-		MembrePersonnel stopState = new MembrePersonnel();
-		stopState.sonStatut(anne);
-
+		anne.seConnecter("anneadherent", "Adherent");
+		anne.achete(tv);
+		anne.payer();
+		/*System.out.println(anne.getSonStat().toString()+ "      "+((Adherent)anne.getSonStat()).getSesCartes().get(0).getPointsDeFidelite());
+		anne.seDeconnecter();
 		System.out.println(anne.getSonStat().toString());
-*/
-		Panier p=new Panier();
-		DirectionMarketing d=new DirectionMarketing();
-		p.addObserver(d);
-		p.ajoutArticle(l1);
-		p.setTotal(100);
-		System.out.println(p.getTotal()+"  "+ p.calculetotal());
-
+		
+		anne.seConnecter("anneperso", "MembrePersonnel");
+		System.out.println(anne.getSonStat().toString());*/
 	}
 
 }
