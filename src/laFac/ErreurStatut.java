b726aacc17eeeb5841147b823000bb3861c7d5a5
chloebrn <44924305+chloebrn@users.noreply.github.com>
2018-12-15 16:17:05 +0100

    Update ErreurStatut.java

--- src/laFac/ErreurStatut.java
+++ src/laFac/ErreurStatut.java
@@ -1,5 +1,7 @@
 package laFac;
 
+//Si un ClientSimple cherche à se déco ou si un clientSimple cherche à se co alors qu'il n'a pas le bon id, etc
+
 public class ErreurStatut extends Exception{
 
 	public ErreurStatut() {
