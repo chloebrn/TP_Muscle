16d51d0fd2f4b5324119084e00e6898548232d05
chloebrn <44924305+chloebrn@users.noreply.github.com>
2018-12-19 22:15:54 +0100

    Add files via upload

---
+++ src/laFac/ErreurProdNonOffrable.java
@@ -0,0 +1,11 @@
+package laFac;
+
+public class ErreurProdNonOffrable extends Exception {
+	public ErreurProdNonOffrable() {
+		super();
+	}
+
+	public ErreurProdNonOffrable(String msg) {
+		super(msg);
+	}
+}
